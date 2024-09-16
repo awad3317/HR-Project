a40acd15e48fb04353daf6016d5dd154b4ec0670
awad3317 <127610663+awad3317@users.noreply.github.com>
2024-09-16 18:15:58 +0300

    edit something

--- database.php
+++ database.php
@@ -1,12 +1,29 @@
 <?php 
+
 class database{
     protected $servername='localhost';
     protected $username='root';
     protected $password='';
     protected $db='hr-project';
+    private $conn;
 
     public function __construct(){
-        
+       $this->conection();
+    }
+    private function conection(){
+       $this->conn= new mysqli($this->servername,$this->username,$this->password,$this->db);
+    }
+    public function delete($table,$id){
+        $result=$this->conn->query("DELETE FROM $table WHERE id =$id");
+
+    }
+    public function All($table){
+        $result=$this->conn->query("SELECT * FROM $table");
+        return $result;
+    }
+    public function select($query){
+        $result=$this->conn->query($query);
+        return $result;
     }
 
 }
